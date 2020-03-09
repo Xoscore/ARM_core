69845cb4350d6e0f482384620842711261e400ef
oleg.gusarov <oleg.gusarov@quoine.com>
2020-03-09 14:06:08 +0700

    Now, this is last commit

--- additonal_tools.py
+++ additonal_tools.py
@@ -1,13 +1,23 @@
 import globals
 
+# To print long lists in comfortable way, should be > 5
+MAX_PRINT_LIST_NUMBER = 10
+
+# Need to point on something
+LIST_POINT_TO = {
+    "personal": "You have ",
+    "uncertain": "There are ",
+    "empty": "",
+}
+
 
 # Usage:
 # Just put any dict or list inside and get string outside
 def tool_list_to_string(some_list, appeal=None, shorten_list=True):
-    if appeal not in globals.LIST_POINT_TO:
-        appeal = globals.LIST_POINT_TO["uncertain"]
+    if appeal not in LIST_POINT_TO:
+        appeal = LIST_POINT_TO["uncertain"]
     else:
-        appeal = globals.LIST_POINT_TO[appeal]
+        appeal = LIST_POINT_TO[appeal]
     if type(some_list) is dict:
         if "name" in some_list:
             list_name = some_list["name"]
@@ -17,7 +27,7 @@ def tool_list_to_string(some_list, appeal=None, shorten_list=True):
             output_string = appeal + "no any " + list_name + "s"
         elif len(some_list["contain"]) == 1:
             output_string = appeal + "one " + list_name + ", it is the " + str(some_list["contain"][0])
-        elif len(some_list["contain"]) > globals.MAX_PRINT_LIST_NUMBER and shorten_list:
+        elif len(some_list["contain"]) > MAX_PRINT_LIST_NUMBER and shorten_list:
             output_string = appeal + ', '.join(map(str, some_list["contain"][:4])) + " and " + \
                             str(len(some_list["contain"]) - 4) + " more " + list_name + "s"
         else:
@@ -29,7 +39,7 @@ def tool_list_to_string(some_list, appeal=None, shorten_list=True):
             output_string = appeal + "no anything like this"
         elif len(some_list) == 1:
             output_string = appeal + "only " + str(some_list[0])
-        elif len(some_list) > globals.MAX_PRINT_LIST_NUMBER and shorten_list:
+        elif len(some_list) > MAX_PRINT_LIST_NUMBER and shorten_list:
             output_string = appeal + ', '.join(map(str, some_list[:4])) + " and " + str(len(some_list) - 4) + \
                   " more of this"
         else:
--- globals.py
+++ globals.py
@@ -4,22 +4,12 @@ LIST_ENGINES = ["presto", "hive"]
 # List of supported formats
 LIST_FORMAT = ["tabular", "csv"]
 
-# Need to point on something
-LIST_POINT_TO = {
-    "personal": "You have ",
-    "uncertain": "There are ",
-    "empty": "",
-}
-
 # Limit for minimum time requests
 MIN_MAX_TIME = 0
 MIN_MIN_TIME = 0
 
 # I need to limit output, because ore then 100 lines has no sense to output in console
 MAX_LINES_OUTPUT = 100
-
-# To print long lists in comfortable way, should be > 5
-MAX_PRINT_LIST_NUMBER = 10
 
 # This set of options come not from requirements, but from my view of what I want from this app if make it for myself
 # If I want to put key on call, not from system
